63543b5932a58bd9b9b37ad307fda3598bfaa3b8
huangjiaqian <18775134221@163.com>
2020-01-05 22:26:03 +0800

    提交

--- LFLiveKitDemo/LFLiveKitDemo/webRTC_ios/PCMFileManager.h
+++ LFLiveKitDemo/LFLiveKitDemo/webRTC_ios/PCMFileManager.h
@@ -14,7 +14,18 @@ NS_ASSUME_NONNULL_BEGIN
 @class LFLiveAudioConfiguration;
 @interface PCMFileManager : NSObject
 
-+ (NSData *)denoiseData:(NSData *)data config: (LFLiveAudioConfiguration *) config;
++ (NSData *)denoiseData:(NSData *)data
+                 config: (LFLiveAudioConfiguration *) config;
+
+/// webRTC降噪
+/// @param buffer 要降噪的音频buf
+/// @param sampleRate 音频采样率
+/// @param samplesCount 总音频采样数
+/// @param level 0 - 3 降噪等级
++(NSData *) nsProcess:(NSData *) data
+           sampleRate:(uint32_t) sampleRate
+         samplesCount:(int) samplesCount
+                level:(int) level;
 
 @end
 
--- LFLiveKitDemo/LFLiveKitDemo/webRTC_ios/PCMFileManager.m
+++ LFLiveKitDemo/LFLiveKitDemo/webRTC_ios/PCMFileManager.m
@@ -56,4 +56,43 @@
     return [NSData dataWithBytes:buffer length:[data length]];
 }
 
+/// webRTC降噪
+/// @param buffer 要降噪的音频buf
+/// @param sampleRate 音频采样率
+/// @param samplesCount 总音频采样数
+/// @param level 0 - 3
++(NSData *) nsProcess:(NSData *)data  sampleRate: (uint32_t) sampleRate samplesCount: (int) samplesCount level: (int) level
+{
+    int16_t *buffer = (int16_t *)[data bytes];
+    if (buffer == 0) return nil;
+    if (samplesCount == 0) return nil;
+    size_t samples = MIN(160, sampleRate / 100);
+    if (samples == 0) return nil;
+    uint32_t num_bands = 1;
+    int16_t *input = buffer;
+    size_t nTotal = (samplesCount / samples);
+    NsHandle *nsHandle = WebRtcNs_Create();
+    int status = WebRtcNs_Init(nsHandle, sampleRate);
+    if (status != 0) {
+        //printf("WebRtcNs_Init fail\n");
+        return nil;
+    }
+    status = WebRtcNs_set_policy(nsHandle, level);
+    if (status != 0) {
+        // printf("WebRtcNs_set_policy fail\n");
+        return nil;
+    }
+    for (int i = 0; i < nTotal; i++) {
+        int16_t *nsIn[1] = {input};   //ns input[band][data]
+        int16_t *nsOut[1] = {input};  //ns output[band][data]
+        WebRtcNs_Analyze(nsHandle, nsIn[0]);
+        WebRtcNs_Process(nsHandle, (const int16_t *const *) nsIn, num_bands, nsOut);
+        input += samples;
+    }
+    WebRtcNs_Free(nsHandle);
+
+    return [NSData dataWithBytes:buffer length:[data length]];
+}
+
+
 @end
